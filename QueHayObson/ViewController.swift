3b354ed2977337c5088c266d81bdef448ed1ad2c
Ernesto Salazar <Neto93@macbook-pro-de-ernesto.local>
2016-10-14 07:57:55 -0700

    Initial Commit

---
+++ QueHayObson/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QueHayObson
+//
+//  Created by Ernesto Salazar on 14/10/16.
+//  Copyright © 2016 DreamTeamCo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
